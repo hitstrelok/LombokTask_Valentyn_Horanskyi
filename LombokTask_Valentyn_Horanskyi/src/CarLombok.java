3fbe36107378ff2cf72649a0271e417257f09b74
hitstrelok <hitstrelok@gmail.com>
2023-10-31 19:21:08 +0100

    Added lombok classpath and modified the class, Valentyn Horanskyi 63500

--- LombokTask_Valentyn_Horanskyi/src/CarLombok.java
+++ LombokTask_Valentyn_Horanskyi/src/CarLombok.java
@@ -1,4 +1,12 @@
+import lombok.Data;
+import lombok.Getter;
+import  lombok.Setter;
+import  lombok.ToString;
+import  lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
 
+
+@Data @AllArgsConstructor @EqualsAndHashCode @Getter @Setter @ToString
 public class CarLombok {
     private String model;
     private String brand;
@@ -11,17 +19,17 @@ public class CarLombok {
 class CarDemoLombok {
     public static void main(String[] args) {
 
-        CarLombok car1 = new CarLombok("GLE","Mercedes",2020,350000, "white", 5);
-        CarLombok car2 = new CarLombok("Chrysler Fifth Avenue","Chrysler",1988,67458,"black", 5);
+        CarLombok car1 = new CarLombok("GLE","Mercedes",2020,350_000, "white", 5);
+        CarLombok car2 = new CarLombok("Chrysler Fifth Avenue","Chrysler",1988,67_458,"black", 4);
 
         System.out.println(car1);
         System.out.println(car2);
 
         System.out.println(car1.getColor());
         int year = car1.getYear();
-        car1.setColor("black");
+        car2.setColor("black");
 
-        System.out.println("Equal?"+ car1.equals(car2));
+        System.out.println("Are they equal: "+ car1.equals(car2));
         System.out.println("Hashcode car1: " + car1.hashCode());
         System.out.println("Hashcode car2: " + car1.hashCode());
 
