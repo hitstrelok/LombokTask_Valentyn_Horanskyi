36514ec2ddab41924b87b366f2117a45744eef97
hitstrelok <hitstrelok@gmail.com>
2023-10-31 20:51:52 +0100

    Made arg brand main in equals(), Valentyn Horanskyi 63500

--- LombokTask_Valentyn_Horanskyi/src/CarLombok.java
+++ LombokTask_Valentyn_Horanskyi/src/CarLombok.java
@@ -1,12 +1,13 @@
-import lombok.Data;
+
 import lombok.Getter;
 import  lombok.Setter;
 import  lombok.ToString;
-import  lombok.EqualsAndHashCode;
 import lombok.AllArgsConstructor;
+import  lombok.EqualsAndHashCode;
 
 
-@Data @AllArgsConstructor @EqualsAndHashCode @Getter @Setter @ToString
+
+@AllArgsConstructor @Getter @Setter @ToString @EqualsAndHashCode(of ="brand")
 public class CarLombok {
     private String model;
     private String brand;
@@ -20,16 +21,16 @@ class CarDemoLombok {
     public static void main(String[] args) {
 
         CarLombok car1 = new CarLombok("GLE","Mercedes",2020,350_000, "white", 5);
-        CarLombok car2 = new CarLombok("Chrysler Fifth Avenue","Chrysler",1988,67_458,"black", 4);
+        CarLombok car2 = new CarLombok("Chrysler Fifth Avenue","Mercedes",1988,67_458,"black", 4);
+        boolean hah = car1.equals(car2);
 
         System.out.println(car1);
         System.out.println(car2);
-
         System.out.println(car1.getColor());
         int year = car1.getYear();
         car1.setColor("black");
 
-        System.out.println("Are they equal: "+ car1.equals(car2));
+        System.out.println("Are they equal: " + car1.equals(car2));
         System.out.println("Hashcode car1: " + car1.hashCode());
         System.out.println("Hashcode car2: " + car1.hashCode());
 
